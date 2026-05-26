a219a5db5f331f149bbd81435f3cf0c9a57b6ab4
broken <wangzhezhilu1245@gmail.com>
2026-05-26 19:22:36 +0800

    first commit

---
+++ 并查集/按边合并/test.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+struct DSU {
+    vector<int> fa;
+    vector<int> siz;
+
+    DSU(int n = 0) {
+        init(n);
+    }
+
+    void init(int n) {
+        fa.resize(n + 1);
+        siz.assign(n + 1, 1);
+
+        for (int i = 1; i <= n; i++) {
+            fa[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (fa[x] == x) return x;
+        return fa[x] = find(fa[x]);
+    }
+
+    bool merge(int x, int y) {
+        int fx = find(x);
+        int fy = find(y);
+
+        if (fx == fy) return false;
+
+        if (siz[fx] > siz[fy]) {
+            swap(fx, fy);
+        }
+
+        fa[fx] = fy;
+        siz[fy] += siz[fx];
+
+        return true;
+    }
+
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    int size(int x) {
+        return siz[find(x)];
+    }
+};
